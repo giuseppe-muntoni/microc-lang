e161daeb717419b3f06c5492b9f2b0e69dda816b
GiuseppeMuntoni <g.muntoni@consorziometis.it>
2024-01-12 11:44:10 +0100

    Added new tests for code generation

---
+++ test/unit/backend/codegen_test.ml
@@ -0,0 +1,24 @@
+open OUnit2
+open Microc
+open Test_utils
+
+let well_formed_module_test sample _ = 
+  let program = Utils.create_ast sample in 
+  let program = Semantic_analysis.check_semantic program in 
+  let llmodule = Codegen.to_llvm_module program in 
+  let llmodule = Optimizer.optimize_module llmodule in 
+  let result = Llvm_analysis.verify_module llmodule in 
+  match result with 
+  | None -> ()
+  | Some error -> assert_bool error false
+  
+let successful_tests = 
+  let files = Array.to_list (Sys.readdir "../../../../../test/samples") in 
+  let files = List.filter (String.starts_with ~prefix: "test-") files in 
+  let tests = List.map (
+    fun file -> 
+      (String.concat " " ["well-formed module"; file]) >:: (well_formed_module_test file)
+  ) files in 
+  "Successful tests for module Codegen" >::: tests
+
+let _ = run_test_tt_main successful_tests;
