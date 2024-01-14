630a2faf6fe845421286a3b6bd045f86585776ff
GiuseppeMuntoni <g.muntoni@consorziometis.it>
2024-01-14 18:53:55 +0100

    added print

--- bin/rt-support.c
+++ bin/rt-support.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 
 // TO BE COMPLETED
-int getint(){
-    char buffer[32];
-    if(fgets(buffer, 32, stdin) == NULL)
-      return 0;
-    return atoi(buffer);
+int getint() {
+  char buffer[32];
+  if(fgets(buffer, 32, stdin) == NULL)
+    return 0;
+  return atoi(buffer);
+}
+
+void print(int x) {
+  printf("%d\n", x);
 }
