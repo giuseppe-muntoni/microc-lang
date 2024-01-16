b145237e0e8109aa6fed95991399791030d4c1e3
GiuseppeMuntoni <g.muntoni@consorziometis.it>
2024-01-16 15:18:47 +0100

    added print float and endline

--- bin/rt-support.c
+++ bin/rt-support.c
@@ -12,3 +12,11 @@ int getint() {
 void print(int x) {
   printf("%d\n", x);
 }
+
+void print_float(float x) {
+  printf("%.4f\n", x);
+}
+
+void print_endline(char str[]) {
+  printf("%s\n", str);
+}
